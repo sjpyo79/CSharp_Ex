1a9005046596ad0da0c56346590645a6b84c5bd5
Primo Pyo <sjpyo79@gmail.com>
2020-05-06 22:22:23 +0900

    초기 커밋.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello");
+            Console.WriteLine("Hello World!");
+            Console.Write("Your name?");
+
+            string name = Console.ReadLine();
+            Console.Write("Hi, ");
+            Console.Write(name);
+            Console.WriteLine (" sir!");
+
+            Console.Write("Age?");
+            var age = int.Parse(Console.ReadLine());
+            Console.Write("Your age is ");
+            Console.WriteLine(age);
+
+            Console.Write("Nick name char?");
+            char 별칭 = char.Parse(Console.ReadLine());
+            Console.WriteLine(별칭);
+        }
+    }
+}
