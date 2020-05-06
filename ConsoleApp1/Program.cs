3bcf17def4fcaabed2ec92f811b1aa8b1b602eb2
Primo Pyo <sjpyo79@gmail.com>
2020-05-06 23:35:10 +0900

    주석 처리 1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,25 +6,29 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.Write("Hello");
-            Console.WriteLine("Hello World!");
-            Console.Write("Your name?");
+            //Console.Write("Hello");
+            //Console.WriteLine("Hello World!");
+            //Console.Write("Your name?");
 
-            string name = Console.ReadLine();
-            Console.Write("Hi, ");
-            Console.Write(name);
-            Console.WriteLine (" sir!");
+            //string name = Console.ReadLine();
+            //Console.Write("Hi, ");
+            //Console.Write(name);
+            //Console.WriteLine (" sir!");
 
-            Console.Write("Age?");
-            var age = int.Parse(Console.ReadLine());
-            Console.Write("Your age is ");
-            Console.WriteLine(age);
+            //Console.Write("Age?");
+            //var age = int.Parse(Console.ReadLine());
+            //Console.Write("Your age is ");
+            //Console.WriteLine(age);
 
-            Console.Write("Nick name char?");
-            char 별칭 = char.Parse(Console.ReadLine());
-            Console.WriteLine(별칭);
+            //Console.Write("Nick name char?");
+            //char 별칭 = char.Parse(Console.ReadLine());
+            //Console.WriteLine(별칭);
 
-            Console.Write("Yes");
+            Console.Write("Yes\n");
+            string noes = @"c:\Docs\dfef";
+            Console.WriteLine(noes);
+
+
         }
     }
 }
